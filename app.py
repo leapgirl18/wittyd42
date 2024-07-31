6a1abce4b9fb10e4d73ecfdcdb77c19e79c87c06
leapgirl18 <dblessed29@verizon.net>
2024-07-30 21:59:21 -0700

    Update app.py to use environment variables for AWS credentials

--- app.py
+++ app.py
@@ -1,45 +1,43 @@
-import boto3
-import json
+# Import necessary libraries
 import os
+import boto3
 from flask import Flask, request, jsonify
+
+# This is a test comment to force a change
 
 app = Flask(__name__)
 
+# Set up S3 client
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
+
+# Endpoint to generate a pre-signed URL
 @app.route('/generate-url', methods=['POST'])
 def generate_url():
-    bucket_name = 'dream29'
+    data = request.get_json()
+    email = data['email']
+    order_id = data['order_id']
+
+    bucket_name = 'your-bucket-name'
     object_key = 'Dream-PatywyEbook-Bluept7.15.pdf'
-    expiration = 900  # URL expiration time in seconds (15 minutes)
-    
+    expiration = 3600
+
     try:
-        # Retrieve AWS credentials from environment variables
-        aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-        aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-        
-        # Initialize the S3 client with the retrieved credentials
-        s3_client = boto3.client(
-            's3',
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key
-        )
-        
-        # Generate pre-signed URL
-        presigned_url = s3_client.generate_presigned_url(
+        response = s3_client.generate_presigned_url(
             'get_object',
             Params={
                 'Bucket': bucket_name,
                 'Key': object_key,
-                'ResponseContentDisposition': 'attachment',
+                'ResponseContentDisposition': f'attachment; filename="{order_id}_Dream-PatywyEbook-Bluept7.15.pdf"'
             },
-            ExpiresIn=expiration  # Use ExpiresIn to specify expiration in seconds
+            ExpiresIn=expiration
         )
-        
-        # Return the pre-signed URL as part of the response
-        return jsonify({'url': presigned_url})
-    
+        return jsonify({'url': response})
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
