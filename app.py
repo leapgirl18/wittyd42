9c948011d51a159f129ade37f2c8f42b52c3d62d
leapgirl18 <dblessed29@verizon.net>
2024-07-30 23:24:17 -0700

    Update app.py with correct bucket name

--- app.py
+++ app.py
@@ -8,21 +8,19 @@ app = Flask(__name__)
 # Retrieve AWS credentials from environment variables
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-aws_region = os.environ.get('AWS_DEFAULT_REGION')
 
-# Initialize S3 client with credentials and region
+# Initialize S3 client with credentials
 s3_client = boto3.client('s3', 
                          aws_access_key_id=aws_access_key_id,
-                         aws_secret_access_key=aws_secret_access_key,
-                         region_name=aws_region)
+                         aws_secret_access_key=aws_secret_access_key)
 
 @app.route('/generate-url', methods=['POST'])
 def generate_url():
     data = request.get_json()
     email = data.get('email')
     order_id = data.get('order_id')
-    bucket_name = 'dream29'  # Your bucket name
-    object_key = 'Dream-PatywyEbook-Bluept7.15.pdf'  # Path to your ebook
+    bucket_name = 'dream29'
+    object_key = 'Dream-PatywyEbook-Bluept7.15.pdf'
 
     try:
         # Generate pre-signed URL for S3 object
@@ -40,7 +38,4 @@ def generate_url():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-# This is a test comment to force a change
+if __name__ == 
