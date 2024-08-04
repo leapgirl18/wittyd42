d77df5ae4b43317e8805faa14c7cee5583bbbc56
leapgirl18 <hello@godswittygirls.com>
2024-08-03 20:24:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-import boto3
-import json
 import os
 from flask import Flask, request, jsonify
+import requests
+import boto3
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Service is up and running!'
 
 @app.route('/', methods=['GET'])
 def home():
@@ -12,22 +16,14 @@ def home():
 @app.route('/generate-url', methods=['POST'])
 def generate_url():
     data = request.get_json()
-    email = data.get('email')
     order_id = data.get('order_id')
     bucket_name = 'dream29'
     object_key = 'Dream-PatywyEbook-Bluept7.15.pdf'
-    
+
     aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-    if not aws_access_key_id or not aws_secret_access_key:
-        missing_credentials = {
-            'AWS_ACCESS_KEY_ID': 'missing' if not aws_access_key_id else 'present',
-            'AWS_SECRET_ACCESS_KEY': 'missing' if not aws_secret_access_key else 'present'
-        }
-        return jsonify({'error': 'Unable to locate credentials', 'details': missing_credentials}), 500
-
-    s3_client = boto3.client('s3', 
+    s3_client = boto3.client('s3',
                              aws_access_key_id=aws_access_key_id,
                              aws_secret_access_key=aws_secret_access_key)
 
@@ -38,11 +34,11 @@ def generate_url():
             Params={
                 'Bucket': bucket_name,
                 'Key': object_key,
-                'ResponseContentDisposition': f'attachment; filename="{order_id}_Dream-PatywyEbook-Bluept7.15.pdf"',
+                'ResponseContentDisposition': 'attachment; filename="Dream-PatywyEbook-Bluept7.15.pdf"'
             },
             ExpiresIn=3600
         )
-        return jsonify({'url': presigned_url})
+        return jsonify({'url': presigned_url}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -51,12 +47,20 @@ def generate_url():
 def thrivecart_webhook():
     if request.method == 'HEAD':
         return '', 200
-    
+
     data = request.get_json()
-    # Process the webhook data here
-    # Add your processing code here
-    print("Received ThriveCart webhook data:", data)
-    return jsonify({'status': 'success'}), 200
+    # Add the Authorization header
+    headers = {
+        'Authorization': 'your_secret_token',
+        'Content-Type': 'application/json'
+    }
+
+    # Forward the request to the AWS API Gateway endpoint
+    aws_api_gateway_url = 'https://91zswqzovj.execute-api.us-east-1.amazonaws.com/Prod/thrivecart-webhook'
+    response = requests.post(aws_api_gateway_url, headers=headers, json=data)
+    
+    # Return the response from AWS API Gateway
+    return jsonify(response.json()), response.status_code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
